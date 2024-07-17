6a4b896f0a8f9f9a7e77a887f6cbb039061732c9
Anshu Rai <raianshu8991@gmail.com>
2024-07-17 11:42:13 +0530

    Added error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import errorHandler from './middleware/error.handler';
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(errorHandler);
 
 // Health check endpoint
 app.get('/health', (req: Request, res: Response) => {
